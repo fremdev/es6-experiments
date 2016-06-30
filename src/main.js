85198bba6ea8b48f90d41ba12f21490efc8687eb
Alex Efremov <alex.efremov.dev@gmail.com>
2016-06-30 23:13:18 +0300

    Add variables module

--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
-console.log('Test');
+import { varCanChange, CONST_EXAMPLE } from './modules/variables';
+console.log(`This is my let variable: ${varCanChange},\nand this is my const variable: ${CONST_EXAMPLE}.\nLet's move on!`);
---
+++ src/modules/variables.js
@@ -0,0 +1,13 @@
+/*
+We can use let keyword for creating a variable.
+It's possible to change the value of the variable later:
+*/
+let varCanChange = 'Now there is some text';
+
+/*
+We use const keyword when we don't want to change the value
+of the variable in future:
+*/
+const CONST_EXAMPLE = 'You can\'t change this';
+
+export { varCanChange, CONST_EXAMPLE }
